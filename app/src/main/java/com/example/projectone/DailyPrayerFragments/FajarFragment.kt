f5023ff2fb725e762bc2d58b635d749183b44d2c
Boo2911 <khushbuswami55@gmail.com>
2023-10-14 19:28:30 +0530

    Initial Commit Commit

--- app/src/main/java/com/example/projectone/DailyPrayerFragments/FajarFragment.kt
+++ app/src/main/java/com/example/projectone/DailyPrayerFragments/FajarFragment.kt
@@ -9,6 +9,8 @@ import com.example.projectone.R
 
 class FajarFragment : Fragment() {
 
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
